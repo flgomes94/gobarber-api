724aa245eba0cee5f3a7ed83ffd3ffaf64fe859b
Fabio Lopes <fabiolopes.dev@gmail.com>
2020-08-15 05:52:04 -0300

    Insert Avatar to User Account

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,8 +1,13 @@
 import { Router } from 'express';
 
+import multer from 'multer';
+import uploadConfig from '../config/upload';
 import CreateUserService from '../services/createUserService';
+import UpdateUserAvatarService from '../services/UpdateUserAvatarService';
+import ensureAuthenticated from '../middlewares/EnsureAuthenticated';
 
 const usersRouter = Router();
+const upload = multer(uploadConfig);
 
 usersRouter.post('/', async (request, response) => {
   try {
@@ -20,4 +25,22 @@ usersRouter.post('/', async (request, response) => {
   }
 });
 
+usersRouter.patch(
+  '/avatar',
+  ensureAuthenticated,
+  upload.single('avatar'),
+  async (request, response) => {
+    try {
+      const updateUserAvatar = new UpdateUserAvatarService();
+      const user = await updateUserAvatar.execute({
+        user_id: request.user.id,
+        avatarFilename: request.file.filename,
+      });
+      delete user.password;
+      return response.json(user);
+    } catch (err) {
+      return response.status(400).json({ error: err.message });
+    }
+  },
+);
 export default usersRouter;
