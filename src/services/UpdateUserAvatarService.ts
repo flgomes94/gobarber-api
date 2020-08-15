d043655867e78ccbfc5a4b3d9aef8bd471bbd1a5
Fabio Lopes <fabiolopes.dev@gmail.com>
2020-08-15 06:39:19 -0300

    Insert Error Exception handler Globally on Project

--- src/services/UpdateUserAvatarService.ts
+++ src/services/UpdateUserAvatarService.ts
@@ -4,6 +4,8 @@ import fs from 'fs';
 import User from '../models/User';
 import uploadConfig from '../config/upload';
 
+import AppError from '../errors/AppError';
+
 interface Request {
   user_id: string;
   avatarFilename: string;
@@ -14,7 +16,7 @@ class UpdateUserAvatarService {
     const usersRepository = getRepository(User);
     const user = await usersRepository.findOne(user_id);
     if (!user) {
-      throw new Error('Only authenticated users can change avatar');
+      throw new AppError('Only authenticated users can change avatar', 401);
     }
     if (user.avatar) {
       const userAvatarFilePath = path.join(uploadConfig.directory, user.avatar);
